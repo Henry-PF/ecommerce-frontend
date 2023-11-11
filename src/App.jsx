4da5d1218311c9630eab33fe3ca2e6c462646278
MatiTar <Matiastari@outlook.com.ar>
2023-11-11 16:04:43 -0300

    Formulario de creacion de Productos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from './components/LandingPage/Home/Home'
 import ProductList from './Components/ProductList/ProductList';
 import './App.css'
+import CreateProduct from './Components/CreateProduct/CreateProduct';
 
 axios.defaults.baseURL = 'http://localhost:3002/api'
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/CreateProducts' element={<CreateProduct/>} />
         <Route exact path='/product_list' element={<ProductList />} />
       </Routes >
     </div>
