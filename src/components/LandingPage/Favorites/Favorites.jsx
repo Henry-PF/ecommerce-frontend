7ab644a0a60461e5e8738f47b9570902ea7e5c44
alexander torres <alexander20012@hotmail.com>
2023-11-21 16:31:52 -0400

    cambios

--- src/components/LandingPage/Favorites/Favorites.jsx
+++ src/components/LandingPage/Favorites/Favorites.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import {React,  useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Offcanvas } from 'react-bootstrap'
 import { BsBagPlus, BsTrash3 } from "react-icons/bs";
--- src/components/LandingPage/Navbar/NavBar.jsx
+++ src/components/LandingPage/Navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import {React,  useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { BsBag, BsHeart, BsPerson, BsSearch } from 'react-icons/bs'
