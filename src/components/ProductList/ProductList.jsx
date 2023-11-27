931581b36621c3c125eee9a96de44b3a37a7172b
Alexander Pedro Jose Torres Apontes <alexander20012@hotmail.com>
2023-11-26 20:20:01 -0400

    cambios

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,7 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import NavBar from '../LandingPage/Navbar/NavBar';
 import Newsletter from '../LandingPage/Newsletter/Newsletter';
